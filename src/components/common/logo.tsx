4676d5957dc2e98d1946bbf6cd9db3c1a4fa94ed
Muhammad Asim <muhammadasim.code@gmail.com>
2025-12-29 11:02:38 -0800

    header and footer is fixed

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 function Logo({ isWhiteVersion = false }: {
-    isWhiteVersion: boolean
+    isWhiteVersion?: boolean
 }) {
     return (
 
